df4d5de345722e5e0fe0a5914d553305af07c5c3
kuwan barnes <Kuwan.barne@gmail.com>
2021-02-09 14:59:01 -0600

    made contact obj

--- src/searchContacts.java
+++ src/searchContacts.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -5,14 +6,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class searchContacts {
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
         Scanner searchContacts= new Scanner(System.in);
         System.out.println("Enter a name to search");
-
+        String nameSearch=searchContacts.nextLine();
+        Path contactPath = Paths.get("contactList.txt");
+        List<String> contactList = Files.readAllLines(contactPath);
+        for (int i = 0; i < contactList.size(); i += 1) {
+            if(nameSearch==)
+            System.out.println((i + 1) + ": " + contactList.get(i));
     }
-//        Path contactPath = Paths.get("contactList.txt");
-//        List<String> contactList = Files.readAllLines(contactPath);
-//        for (int i = 0; i < contactList.size(); i += 1) {
-//            System.out.println((i + 1) + ": " + contactList.get(i));
+//
+//
     }
 }
