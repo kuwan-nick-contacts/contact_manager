e92cd0d57edd046316147a4d3523d03c57515135
kuwan barnes <Kuwan.barne@gmail.com>
2021-02-10 10:41:45 -0600

    search

--- src/searchContacts.java
+++ src/searchContacts.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,17 +10,38 @@ public class searchContacts {
 
 
     public static void main(String[] args) throws IOException {
-        Scanner searchContacts= new Scanner("contactList.txt");
+        Scanner searchContacts= new Scanner(System.in);
         System.out.println("Enter a name to search");
         String nameSearch=searchContacts.nextLine();
         Path contactPath = Paths.get("contactList.txt");
         List<String> contactList = Files.readAllLines(contactPath);
-        for (int i = 0; i < contactList.size(); i += 1) {
-            if(nameSearch==)
+        int linecount= 0;
+        String line;
+        boolean noMatches = true;
+        System.out.println(contactList);
+        if (contactList.contains(nameSearch)){
+            System.out.println("found");
+        }
+//        while ((line = searchContacts.nextLine()) != null) {
+//            if(Arrays.asList(contactPath).contains(nameSearch)){
+//                System.out.println(Arrays.asList(contactPath));
+//            }
+//            // Increment the count and find the index of the word.
+//            linecount++;
+//            int indexFound = line.indexOf(nameSearch.toLowerCase());
+//            System.out.println(nameSearch);
+//            // If greater than -1, means we found a match.
+//            if (indexFound > -1) {
+//                System.out.println("\nContact was FOUND\n"
+//                        + "\nContact " + linecount + ": " + line);
+//                noMatches = false;
+//            }
 
-            System.out.println((i + 1) + ": " + contactList.get(i));
+
+
+//            System.out.println((i + 1) + ": " + contactList.get(i));
     }
 //
 //
     }
-}
+
