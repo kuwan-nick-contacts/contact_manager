b6375f6311555b537ac2acfc1e635ec4c2147e5c
kuwan barnes <Kuwan.barne@gmail.com>
2021-02-10 12:07:02 -0600

    fixed delete

--- src/DeleteContact.java
+++ src/DeleteContact.java
@@ -1,22 +1,36 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class DeleteContact {
-    Scanner deleteScanner = new Scanner(System.in);
-
-    List<String> lines = Files.readAllLines(Paths.get("data", "contactlist.txt"));
-    List<String> newList = new ArrayList<>();
-
-for (String line : lines) {
-        if (line.equals("milk")) {
-            newList.add("cream");
-            continue;
-        }
-        newList.add(line);
+    public  DeleteContact(){
     }
 
-Files.write(Paths.get("data", "contactlist.txt"), newList);
+    public static void main(String[] args) throws IOException {
+
+
+        Scanner deleteScanner = new Scanner(System.in);
+        System.out.println("enter contact to delete");
+        String delete = deleteScanner.nextLine();
+
+        List<String> lines = Files.readAllLines(Paths.get( "contactList.txt"));
+        List<String> newList = new ArrayList<>();
+
+        for (String line : lines) {
+            if (line.contains(delete)) {
+                newList.add(" ");
+                continue;
+            }
+            newList.add(line);
+        }
+        Files.write(Paths.get("contactList.txt"), newList);
+
+
+
+
+    }
 }
