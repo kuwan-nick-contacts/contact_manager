61081af2dc728aca25dcb9845d2a86a42bc15b6c
kuwan barnes <Kuwan.barne@gmail.com>
2021-02-11 10:02:01 -0600

    fixed add

--- src/Main_menu.java
+++ src/Main_menu.java
@@ -1,8 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
-public class Main_menu extends viewContacts {
+public class Main_menu  {
 
-    public static void main(String[] args) {
+    public static void viewContacts() throws IOException {
+        Path contactPath = Paths.get("contactList.txt");
+        List<String> contactList = Files.readAllLines(contactPath);
+        for (int i = 0; i < contactList.size(); i += 1) {
+            System.out.println((i + 1) + ": " + contactList.get(i));
+
+        }
+    }
+    public static class addContact {
+       public String firstName;
+        public String lastName;
+        public String phoneNumber;
+
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
         System.out.println("What would you like to do?\n" +
                 "1. View contacts.\n" +
                 "2. Add a new contact.\n" +
@@ -16,11 +40,30 @@ public class Main_menu extends viewContacts {
             System.out.println("invalid selection");
             main(args);
         }else if(menuOption==1){
-            viewContacts viewContacts;
-            viewContacts = new viewContacts();
+
+             viewContacts();
         }else if(menuOption==2){
-        new addContact();
-    }else if(menuOption==3){
+            String firstName;
+            String lastName;
+            String phoneNumber;
+            Scanner addCon= new Scanner(System.in);
+            System.out.println("Please enter Contact info.");
+            String added= addCon.nextLine();
+            String[] array =added.split(" ");
+            added.split(" ");
+            Contact newC = new Contact();
+            newC.setFirstName(firstName=array[0]);
+            newC.setLastName(lastName=array[1]);
+            newC.setPhoneNumber(phoneNumber=array[2]);
+            System.out.println(Arrays.toString(added.split(" ")));
+            Path filePath = Paths.get("contactList.txt");
+            List<String> contact = Arrays.asList(
+                    added
+            );
+            Files.write(filePath,contact , StandardOpenOption.APPEND);
+
+
+        }else if(menuOption==3){
         new searchContacts();
         }else if(menuOption==4){
             new DeleteContact();
@@ -35,4 +78,5 @@ public class Main_menu extends viewContacts {
 //5. Exit.
 //Enter an option (1, 2, 3, 4 or 5):
     }
+
 }
